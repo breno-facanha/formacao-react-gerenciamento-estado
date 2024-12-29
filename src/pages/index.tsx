1853f6ec949163a619ae1a5c31b36bb93aeb9249
Breno Façanha <brenofacanha.ti@gmail.com>
2024-12-29 15:28:04 -0300

    criado o titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,21 @@ export default function Home() {
   return (
     <div className="
     flex flex-col justify-center items-center h-screen
-    bg-gradient-to-br from-zinc-800 to-black
+    bg-gradient-to-br from-zinc-800 to-black gap-20
     ">
+
+      <div className="text-6xl">
+        <span className="opacity-40 font-thin">Gerenciamento de </span>
+        <span className="
+          font-black bg-gradient-to-r from-blue-500 to-green-500
+          text-transparent bg-clip-text
+          ">Estado</span>
+      </div>
+
       <div>
         <MenuItem icone={<IconForms />} texto="Estado" url="/revisao/estado"/>
       </div>
+
     </div>
   );
 }
